b01f494ddd102585af9244764395f69e6ac8a686
Eppleton IT <seal@dukescript.com>
2019-08-04 06:16:38 +0200

    Add button is disabled on empty description

--- src/main/java/com/kt/mvvm/demo/Demo.java
+++ src/main/java/com/kt/mvvm/demo/Demo.java
@@ -25,6 +25,7 @@ package com.kt.mvvm.demo;
 
 import com.dukescript.api.javafx.beans.FXBeanInfo;
 import javafx.beans.binding.Bindings;
+import javafx.beans.binding.BooleanBinding;
 import javafx.beans.binding.IntegerBinding;
 import javafx.beans.property.ListProperty;
 import javafx.beans.property.SimpleListProperty;
@@ -35,6 +36,7 @@ import static net.java.html.json.Models.applyBindings;
 
 public final class Demo implements FXBeanInfo.Provider {
     private final StringProperty desc = new SimpleStringProperty(this, "desc", "Buy Milk");
+    private final BooleanBinding emptyDesc = Bindings.equal(desc, "");
     private final ListProperty<String> todos = new SimpleListProperty<>(this, "todos", FXCollections.observableArrayList());
     private final IntegerBinding numTodos = Bindings.createIntegerBinding(todos::size, todos);
 
@@ -45,6 +47,7 @@ public final class Demo implements FXBeanInfo.Provider {
 
     private final FXBeanInfo info = FXBeanInfo.newBuilder(this)
             .property(desc)
+            .property("emptyDesc", emptyDesc)
             .property(todos)
             .property("numTodos", numTodos)
             .action("addTodo", this::addTodo)
